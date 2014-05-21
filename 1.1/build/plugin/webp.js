a24482ab9ad7f6e9054cad615eef38bb2d688377
qingkun.liao <qingkun.liao@taobao.com>
2014-05-21 15:03:28 +0800

    修正webp判断代码的问题

--- 1.1/build/plugin/webp.js
+++ 1.1/build/plugin/webp.js
@@ -8,11 +8,6 @@ gallery/datalazyload/1.1/plugin/webp
 // https://github.com/sorrycc/webp-support/blob/master/lib/webp.js
 KISSY.add('gallery/datalazyload/1.1/plugin/webp',function (S) {
     function isSupportStorage(cb) {
-        // webkit内核的浏览器支持webp，如safari，所以下面的判断不正��?
-//        if ((!window.chrome && !window.opera)) {
-//            cb(false);
-//            return;
-//        }
         var val = window.localStorage && window.localStorage.getItem('webpsupport');
         if (val !== null) {
             cb(val === 'true');
@@ -25,12 +20,20 @@ KISSY.add('gallery/datalazyload/1.1/plugin/webp',function (S) {
     }
 
     function isSupportTest(cb) {
-        var img = new Image();
+		var img = new Image(), loaded;
+		img.onload = img.onerror = function () {
+			if (!loaded) {
+				loaded = true;
+				cb(img.width === 2 && img.height === 2);
+			}
+		};
+		setTimeout(function () {
+			if (!loaded) {
+				loaded = true;
+				cb(false);
+			}
+		}, 16);
         img.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-        img.onload = img.onerror = function () {
-            S.log('webp is supported!');
-            cb(img.width === 2 && img.height === 2);
-        };
     }
 
     var WebP = window.WebP = {};
